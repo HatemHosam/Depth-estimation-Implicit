13d7065f2ca68fbbae24ffab8eef40994759e1e0
Hatem Ibrahem <48507516+HatemHosam@users.noreply.github.com>
2024-04-16 19:06:13 -0500

    Update gen_data.py

--- gen_data.py
+++ gen_data.py
@@ -4,32 +4,32 @@ import os
 from random import shuffle
 import numpy as np
 
-img_dir = '/data/i5O/nyudepthv2/train/'
-img_folders = os.listdir(img_dir)
-h5_list = []
+#img_dir = '/data/i5O/nyudepthv2/train/'
+#img_folders = os.listdir(img_dir)
+#h5_list = []
 
-for folder in img_folders:
-    for h5_file in os.listdir(img_dir+folder):
-        h5_list.append(img_dir+folder+'/'+h5_file)
+#for folder in img_folders:
+#    for h5_file in os.listdir(img_dir+folder):
+#        h5_list.append(img_dir+folder+'/'+h5_file)
         
 #print(h5_list)		
-shuffle(h5_list)
+#shuffle(h5_list)
 
-i = 0
-for file in h5_list:
-    i += 1
-    with h5py.File(file, "r") as f:
-        depth = f['depth']
-        depth = (depth[:]).astype('float')
-        np.save('/data/i5O/nyudepthv2_data/train/depth/'+file.split('/')[-2]+'_'+file.split('/')[-1].replace('.h5','.npy'), depth)
+#i = 0
+#for file in h5_list:
+#    i += 1
+#    with h5py.File(file, "r") as f:
+#        depth = f['depth']
+#        depth = (depth[:]).astype('float')
+#        np.save('/data/i5O/nyudepthv2_data/train/depth/'+file.split('/')[-2]+'_'+file.split('/')[-1].replace('.h5','.npy'), depth)
 		
-        img = np.transpose(f['rgb'], (1, 2, 0))
-        img = np.array(img, dtype = np.uint8)
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-        cv2.imwrite('/data/i5O/nyudepthv2_data/train/image/'+file.split('/')[-2]+'_'+file.split('/')[-1].replace('.h5','.jpg'), img)
+#        img = np.transpose(f['rgb'], (1, 2, 0))
+#        img = np.array(img, dtype = np.uint8)
+#        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+#        cv2.imwrite('/data/i5O/nyudepthv2_data/train/image/'+file.split('/')[-2]+'_'+file.split('/')[-1].replace('.h5','.jpg'), img)
     
-    if i > 20000:
-        break
+#    if i > 20000:
+#        break
 
 img_dir_test = '/data/i5O/nyudepthv2/val/official/'
 img_folders_test = os.listdir(img_dir_test)
