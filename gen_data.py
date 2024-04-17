75af31f81584b8003c345f4d032f9583b3352feb
Hatem Ibrahem <48507516+HatemHosam@users.noreply.github.com>
2024-04-16 19:10:58 -0500

    Update gen_data.py

--- gen_data.py
+++ gen_data.py
@@ -36,8 +36,8 @@ img_folders_test = os.listdir(img_dir_test)
 h5_list_test = []
 
 for folder in img_folders_test:
-    for h5_file in os.listdir(img_dir_test+folder):
-        h5_list_test.append(img_dir_test+folder+'/'+h5_file)
+    #for h5_file in os.listdir(img_dir_test+folder):
+    h5_list_test.append(img_folders_test+folder+'/'+h5_file)
         
 #print(h5_list_test)		
 shuffle(h5_list_test)
